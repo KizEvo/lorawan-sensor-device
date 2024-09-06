d8d33a527f735f3186eaae86dfd53b4d76efbcc0
Duc Phu <vandungqc03@gmail.com>
2024-09-06 17:20:48 +0700

    add aes128 and aes-cmac crypto

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -26,6 +26,12 @@
 /* USER CODE BEGIN Includes */
 #include "LoRa.h"
 #include "dht11.h"
+#include "aes.h"
+#include "cmac.h"
+#include "encrypt.h"
+#include "secrets.h"
+#include <stdlib.h>
+#include <string.h>
 //#include "secrets.h"
 //#include "cc20_p1305.h"
 /* USER CODE END Includes */
@@ -60,10 +66,27 @@ void SystemClock_Config(void);
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 
-LoRa myLoRa;
-uint16_t LoRa_stat = 0;
+#define SLEEP_COUNT_MAX 5
 
-dht11 myDHT11;
+/* the assumption is that FOPTS field is absent and payload is 5 bytes max => 18 */
+enum lorawan_mac_frame_offset {LORAWAN_MAC_HDR = 0, LORAWAN_DEVADDR = 1, LORAWAN_FCTRL = 5, LORAWAN_FCNT = 6, LORAWAN_FPORT = 8, LORAWAN_FRMPAYLOAD = 9, LORAWAN_MIC = 14};
+
+static LoRa myLoRa;
+static uint16_t LoRa_stat = 0;
+
+static dht11 myDHT11;
+
+// LoRaWAN NwkSKey, network session key
+// This should be in big-endian (aka msb).
+static uint8_t NWKSKEY[] = { NWKSKEY1 };
+
+// LoRaWAN AppSKey, application session key
+// This should also be in big-endian (aka msb).
+static uint8_t APPSKEY[] = { APPSKEY1 };
+
+static const uint8_t DEVADDR[] = { DEV_ADDR1_BYTE }; // Change this address for every node
+
+static uint8_t lorawan_frame_to_calc_mic[18] = {0};
 
 void TIM4_EnablePeripheral(void)
 {
@@ -131,6 +154,83 @@ void led_flashing(GPIO_TypeDef *port, uint16_t pin, uint8_t time)
 		HAL_Delay(200);
 	}
 }
+
+/* total byte = 14 */
+int32_t set_msg_frame_for_mic_calc(uint8_t *msg, uint8_t *encrypted_payload, uint16_t fcounter)
+{
+	msg[LORAWAN_MAC_HDR] = 0x40;
+	msg[LORAWAN_DEVADDR] = 0x49;
+	msg[LORAWAN_DEVADDR + 1] = 0x1E;
+	msg[LORAWAN_DEVADDR + 2] = 0x0D;
+	msg[LORAWAN_DEVADDR + 3] = 0x26;
+	msg[LORAWAN_FCTRL] = 0x00;
+	msg[LORAWAN_FCNT] = fcounter & 0xFF;
+	msg[LORAWAN_FCNT + 1] = fcounter >> 8;
+	msg[LORAWAN_FPORT] = 0x1;
+	memcpy(&msg[LORAWAN_FRMPAYLOAD], encrypted_payload, 5);
+	
+	return 0;
+}
+
+int32_t set_block_b_frame_for_mic_calc(uint8_t *b, uint16_t fcounter, uint8_t payload_size)
+{
+	b[0] = 0x49;
+	memset(&b[1], 0x00, 5);
+	b[6] = 0x49;
+	b[7] = 0x1E;
+	b[8] = 0x0D;
+	b[9] = 0x26;
+	b[10] = fcounter & 0xFF;
+	b[11] = fcounter >> 8;
+	b[12] = 0x00;
+	b[13] = 0x00;
+	b[14] = 0x00;
+	b[15] = payload_size;
+	
+	return 0;
+}
+
+int32_t set_actual_data_send(uint8_t *out, uint8_t *msg, uint8_t *mic, uint8_t msg_size)
+{
+    out[0] = msg[LORAWAN_MAC_HDR];
+    out[1] = msg[LORAWAN_DEVADDR];
+    out[2] = msg[LORAWAN_DEVADDR + 1];
+    out[3] = msg[LORAWAN_DEVADDR + 2];
+    out[4] = msg[LORAWAN_DEVADDR + 3];
+    out[5] = msg[LORAWAN_FCTRL];
+    out[6] = msg[LORAWAN_FCNT];
+    out[7] = msg[LORAWAN_FCNT + 1];
+    out[8] = msg[LORAWAN_FPORT];
+    out[9] = msg[LORAWAN_FRMPAYLOAD];
+    out[10] = msg[LORAWAN_FRMPAYLOAD + 1];
+    out[11] = msg[LORAWAN_FRMPAYLOAD + 2];
+    out[12] = msg[LORAWAN_FRMPAYLOAD + 3];
+    out[13] = msg[LORAWAN_FRMPAYLOAD + 4];
+    out[14] = mic[0];
+    out[15] = mic[1];
+    out[16] = mic[2];
+    out[17] = mic[3];
+
+	return 0;
+}
+
+/* Size must be equals to use this func */
+int32_t byte_array_xor(uint8_t *byte_a, uint8_t *byte_b, uint8_t *out, uint32_t size)
+{
+	for (int i = 0; i < size; i++) {
+		out[i] = byte_a[i] ^ byte_b[i];
+	}
+	return 0;
+}
+
+uint8_t msg[14] = {0};
+uint8_t b[16] = {0};
+uint8_t frame_msg[30] = {0};
+uint8_t cmac[16] = {0};
+uint8_t mic[4] = {0};
+
+uint8_t actual_data_send[18];
+						
 /* USER CODE END 0 */
 
 /**
@@ -188,28 +288,77 @@ int main(void)
 		LoRa_stat = 1;
 	}
 	if (LoRa_stat) {
-		LoRa_setSyncWord(&myLoRa, 0xF3);
+		LoRa_setSyncWord(&myLoRa, 0x12);
 	}
 	
 	if (dht11_init(&myDHT11) == 0) {
 		led_flashing(LED_GPIO_Port, LED_Pin, 5);
 	}
 
-  /* USER CODE END 2 */
+	uint8_t sleep_count = 5;
+	uint8_t package_count = 0;
+
+	/* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
 		if (LoRa_stat) {
-			if (dht11_read(&myDHT11) == 0) {
-				led_flashing(LED_GPIO_Port, LED_Pin, 2);
-				if (LoRa_transmit(&myLoRa, myDHT11.data, 5, 500)) {
-					led_flashing(LED_GPIO_Port, LED_Pin, 5);
+			/* 
+			 * total sleep time is 3 mins to comply with 500 pacakges 
+			 * SF7 per day for TTN Fair Use Policy (10 bytes payload)
+			 * => 30 seconds air time per device
+			 * Note :we could increase it up to 1000 packages since
+			 * current payload is 5 bytes only but 
+			 */
+			if (sleep_count >= SLEEP_COUNT_MAX) {
+				
+        uint8_t Ai[16] = {0x01, 0, 0, 0, 0, 0, 0x49, 0x1E, 0x0D, 0x26, 0, 0, 0, 0, 0, 1};
+				Ai[13] = package_count;
+				uint32_t Ai_size = 16;
+				uint8_t *S = ecb_encrypt(Ai, APPSKEY, aes_128_encrypt, &Ai_size);
+				
+				Ai_size = 16;
+        uint8_t payload[16] = {92, 0, 32, 0, 60};
+				uint8_t encrypted_payload[16] = {0};
+				byte_array_xor(payload, S, encrypted_payload, Ai_size);
+
+				memset(msg, 0x00, 14);
+				set_msg_frame_for_mic_calc(msg, encrypted_payload, package_count);
+
+				memset(b, 0x00, 16);
+				set_block_b_frame_for_mic_calc(b, package_count, 14);
+
+
+				memset(frame_msg, 0x00, 30);
+				memcpy(frame_msg, b, 16);
+				memcpy(&frame_msg[16], msg, 14);
+				
+
+				memset(cmac, 0x00, 16);
+				aes_cmac(frame_msg, 30, cmac, NWKSKEY);
+				
+
+				memset(mic, 0x00, 4);
+				memcpy(mic, cmac, 4);
+				
+				memset(actual_data_send, 0x00, 18);
+				set_actual_data_send(actual_data_send, msg, mic, 14);
+				package_count++;
+				free(S);
+				S = NULL;
+				sleep_count = 0;
+				if (dht11_read(&myDHT11) == 0) {
+					led_flashing(LED_GPIO_Port, LED_Pin, 2);
+					if (LoRa_transmit(&myLoRa, actual_data_send, 18, 1000)) {
+						led_flashing(LED_GPIO_Port, LED_Pin, 5);
+					}
+				} else {
+					HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_SET);
 				}
-			} else {
-				HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_SET);
 			}
+			
 		}
 		/* Start timer interrupt */
 		TIM2_Start_IT();
@@ -221,6 +370,8 @@ int main(void)
 		HAL_ResumeTick();
 		/* Disable timer interrupt to process other things */
 		TIM2_Disable_IT();
+		/* increment sleep count */
+		sleep_count++;
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
