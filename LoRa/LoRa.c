92ecefc6c575ed32c8214fbab08b8c9d0bf7bd63
Duc Phu <vandungqc03@gmail.com>
2024-10-27 09:57:36 +0700

    change transmission power to 14dBm

--- LoRa/LoRa.c
+++ LoRa/LoRa.c
@@ -23,7 +23,7 @@ LoRa newLoRa(){
 	new_LoRa.spredingFactor        = SF_7      ;
 	new_LoRa.bandWidth			   = BW_125KHz ;
 	new_LoRa.crcRate               = CR_4_5    ;
-	new_LoRa.power				   = POWER_20db;
+	new_LoRa.power				   = POWER_14db;
 	new_LoRa.overCurrentProtection = 100       ;
 	new_LoRa.preamble			   = 8         ;
 
